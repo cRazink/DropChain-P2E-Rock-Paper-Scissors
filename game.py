97a978c4ab996a77cea93ebdc382905f96a28909
cRazink <50594263+cRazink@users.noreply.github.com>
2023-09-27 20:03:35 -0500

    Update game.py

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ class Game:
         self.moves = [None, None]
         self.wins = [0,0]
         self.ties = 0
+        self.payout_done = False
 
     def get_player_move(self, p):
         """
@@ -29,6 +30,8 @@ class Game:
         return self.p1Went and self.p2Went
 
     def winner(self):
+        if self.payout_done:
+            return -1, None, None, None  # return a placeholder value
 
         p1 = self.moves[0].upper()[0]
         p2 = self.moves[1].upper()[0]
@@ -60,47 +63,12 @@ class Game:
             winner_uid = user2_uid
             loser_uid = user1_uid
         note = "Player 1 chose {0} and player 2 chose {1}.".format(p1,p2)
-        payout_winner(app_id, loser_uid, winner_uid, note)
-        return winner
+        return winner, winner_uid, loser_uid, note
 
     def resetWent(self):
         self.p1Went = False
         self.p2Went = False
-	
-# [ Start DropChain API Integrations ]
-import requests, json
 
-# grabbed from DropChain test user accounts
-user1_uid = "<your Test User ID 1>" # test user uid 1 
-user2_uid = "<your Test User ID 2>" # test user uid 2  
-app_id = "<your App ID>" # app_id 
-
-run_count = 0
-
-def payout_winner(app_id, user1_uid, winner_uid, note): 
-
-    url = "https://api.dropchain.network/v1/send_algo_testnet"
-
-    payload = {
-	"app_id": app_id,
-        "user1_uid": user1_uid,
-        "receiver1_uid": winner_uid,
-        "asset1_amount_int": "100000", # this amount can be changed based on the wager for the game
-        "transaction1_note": note
-    }
-    headers = {
-	    "content-type": "application/json",
-	    "X-API-Key": "<Your DropChain developer account API Key>", # taken from your DropChain API dashboard
-    }
-
-    # ensure function runs 1 time — fixes demo when winner() is called multiple times
-    global run_count
-    if run_count == 0:
-        response = requests.request("POST", url, json=payload, headers=headers)
-        run_count += 1
-        response = json.loads(response.text)
-        print(response)
-        return response
-    else:
-        return
-# [ End DropChain API Integrations ]
+# define below user ids based on player 1 or player 2
+user1_uid = "<Your Test User 1 ID>" # test user uid 1 = player 1 
+user2_uid = "<Your Test User 2 ID>" # test user uid 2 = player 2
